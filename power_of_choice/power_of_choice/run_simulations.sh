6898bcf2a2a39363b6053d58f11d522a754d813d
livialestingi <livia.lestingi@polimi.it>
2025-03-10 07:51:33 +0100

    adding first batch of results

--- power_of_choice/power_of_choice/run_simulations.sh
+++ power_of_choice/power_of_choice/run_simulations.sh
@@ -1,14 +1,37 @@
 #!/bin/bash
 
+python -m dataset_preparation seed=267
 
-python -m power_of_choice.main
-python -m power_of_choice.main variant="base" strategy.d=20 strategy.ck=9
-python -m power_of_choice.main variant="cpow" strategy.d=20 strategy.ck=9
-python -m power_of_choice.main variant="rpow" strategy.d=60 strategy.ck=9
+python -m pow_so optimizer=so variant="rand" seed=267 strategy.d=20 strategy.ck=9
+python -m pow_so optimizer=so variant="base" seed=267 strategy.d=20 strategy.ck=9
+python -m pow_so optimizer=so variant="cpow" seed=267 strategy.d=20 strategy.ck=9
+python -m pow_so optimizer=so variant="rpow" seed=267 strategy.d=20 strategy.ck=9
 
-python -m power_of_choice.pow_so
-python -m power_of_choice.pow_uo optimizer=uo
-python -m power_of_choice.pow_rt optimizer=rt
-python -m power_of_choice.pow_ecto optimizer=ecto
+python -m pow_uo optimizer=uo variant="rand" seed=267 strategy.d=20 strategy.ck=9
+python -m pow_uo optimizer=uo variant="base" seed=267 strategy.d=20 strategy.ck=9
+python -m pow_uo optimizer=uo variant="cpow" seed=267 strategy.d=20 strategy.ck=9
+python -m pow_uo optimizer=uo variant="rpow" seed=267 strategy.d=20 strategy.ck=9
+
+python -m pow_rt optimizer=rt variant="rand" seed=267 strategy.d=20 strategy.ck=9
+python -m pow_rt optimizer=rt variant="base" seed=267 strategy.d=20 strategy.ck=9
+python -m pow_rt optimizer=rt variant="cpow" seed=267 strategy.d=20 strategy.ck=9
+python -m pow_rt optimizer=rt variant="rpow" seed=267 strategy.d=20 strategy.ck=9
+
+python -m dataset_preparation seed=719
+
+python -m pow_so optimizer=so variant="rand" seed=719 strategy.d=20 strategy.ck=9
+python -m pow_so optimizer=so variant="base" seed=719 strategy.d=20 strategy.ck=9
+python -m pow_so optimizer=so variant="cpow" seed=719 strategy.d=20 strategy.ck=9
+python -m pow_so optimizer=so variant="rpow" seed=719 strategy.d=20 strategy.ck=9
+
+python -m pow_uo optimizer=uo variant="rand" seed=719 strategy.d=20 strategy.ck=9
+python -m pow_uo optimizer=uo variant="base" seed=719 strategy.d=20 strategy.ck=9
+python -m pow_uo optimizer=uo variant="cpow" seed=719 strategy.d=20 strategy.ck=9
+python -m pow_uo optimizer=uo variant="rpow" seed=719 strategy.d=20 strategy.ck=9
+
+python -m pow_rt optimizer=rt variant="rand" seed=719 strategy.d=20 strategy.ck=9
+python -m pow_rt optimizer=rt variant="base" seed=719 strategy.d=20 strategy.ck=9
+python -m pow_rt optimizer=rt variant="cpow" seed=719 strategy.d=20 strategy.ck=9
+python -m pow_rt optimizer=rt variant="rpow" seed=719 strategy.d=20 strategy.ck=9
 
 echo "All simulations completed."
