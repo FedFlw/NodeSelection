e6cd79591b5e42ff756cef56f58bd0b7264ab135
livialestingi <livia.lestingi@polimi.it>
2025-03-12 08:47:52 +0100

    adding second batch of results

--- power_of_choice/power_of_choice/run_simulations.sh
+++ power_of_choice/power_of_choice/run_simulations.sh
@@ -1,22 +1,5 @@
 #!/bin/bash
 
-python -m dataset_preparation seed=267
-
-python -m pow_so optimizer=so variant="rand" seed=267 strategy.d=20 strategy.ck=9
-python -m pow_so optimizer=so variant="base" seed=267 strategy.d=20 strategy.ck=9
-python -m pow_so optimizer=so variant="cpow" seed=267 strategy.d=20 strategy.ck=9
-python -m pow_so optimizer=so variant="rpow" seed=267 strategy.d=20 strategy.ck=9
-
-python -m pow_uo optimizer=uo variant="rand" seed=267 strategy.d=20 strategy.ck=9
-python -m pow_uo optimizer=uo variant="base" seed=267 strategy.d=20 strategy.ck=9
-python -m pow_uo optimizer=uo variant="cpow" seed=267 strategy.d=20 strategy.ck=9
-python -m pow_uo optimizer=uo variant="rpow" seed=267 strategy.d=20 strategy.ck=9
-
-python -m pow_rt optimizer=rt variant="rand" seed=267 strategy.d=20 strategy.ck=9
-python -m pow_rt optimizer=rt variant="base" seed=267 strategy.d=20 strategy.ck=9
-python -m pow_rt optimizer=rt variant="cpow" seed=267 strategy.d=20 strategy.ck=9
-python -m pow_rt optimizer=rt variant="rpow" seed=267 strategy.d=20 strategy.ck=9
-
 python -m dataset_preparation seed=719
 
 python -m pow_so optimizer=so variant="rand" seed=719 strategy.d=20 strategy.ck=9
